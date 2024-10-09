a2fd1464cb4b0df9acc14684d71a66db07f88c9f
Berkay Akciger <berkayakcigerr@gmail.com>
2024-10-09 12:46:17 +0300

    Added getallArticle service

--- src/main/java/com/rodiugurlu/starter/manager/ArticleManager.java
+++ src/main/java/com/rodiugurlu/starter/manager/ArticleManager.java
@@ -1,4 +1,22 @@
 package com.rodiugurlu.starter.manager;
 
-public class ArticleManager {
+import com.rodiugurlu.starter.entity.Article;
+import com.rodiugurlu.starter.repository.ArticleRepository;
+import com.rodiugurlu.starter.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleManager implements ArticleService {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+
+    @Override
+    public List<Article> getAllArticles() {
+        return articleRepository.findAll();
+    }
 }
--- src/main/java/com/rodiugurlu/starter/service/ArticleService.java
+++ src/main/java/com/rodiugurlu/starter/service/ArticleService.java
@@ -1,4 +1,10 @@
 package com.rodiugurlu.starter.service;
 
+import com.rodiugurlu.starter.dto.DtoArticle;
+import com.rodiugurlu.starter.entity.Article;
+
+import java.util.List;
+
 public interface ArticleService {
+    List<Article> getAllArticles();
 }
